34f21d9366b27d6a20fd30aa4f22fd0008a9c803
abdoulkder <abdoulkaderkonfe7@gmail.com>
2023-02-18 03:19:19 -0800

    m

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i = 'a';
+
+	while (i <=  'z' && i != 'e' && i != 'q')
+	{
+		putchar (i);
+		i++;
+	}
+	putchar ('\n');
+	return (0);
+}
+
