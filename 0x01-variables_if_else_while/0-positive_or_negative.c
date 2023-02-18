53a51b0dd7a89e5c5b4209143c7a30374f43d134
abdoulkder <abdoulkaderkonfe7@gmail.com>
2023-02-17 16:07:15 -0800

    m

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,9 @@
 #include <stdio.h>
 /* betty style doc for function main goes there */
 /**
- * main -  main fuction This is my main function
- *
- * Return : always 0
+* main - main function
+*
+* Return: this code always returns 0
  */
 int main(void)
 {
