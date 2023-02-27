03640930a374648629072b0b66a47fde0f66f9a2
abdoulkder <abdoulkaderkonfe7@gmail.com>
2023-02-27 08:21:41 -0800

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,25 @@
 #include "main.h"
-
 /**
- * print_rev - function that prints the reversed form of the character
- * @s: character that must be printed in the reversed form
- *
- * Return: always 0
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
  */
 void print_rev(char *s)
 {
 	int longi = 0;
-	int j;
+	int o;
 
 	while (*s != '\0')
 	{
 		longi++;
 		s++;
-	}	
-	for (j = longi - 1; j >= 0; j--)
-		_putchar(*(s + j));
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
 	_putchar('\n');
 }
